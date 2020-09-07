74da8b755db21a1b852ef65dbe355c97837af344
liya <2793955734@qq.com>
2020-09-07 23:10:41 +0800

    脚手架的初次使用

---
+++ src/components/Second.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p>
+            这是我的第二个组件
+        </p>
+        {{username}}
+        <hr>
+        <span v-model="count">{{count}}</span>
+        <button @click="change">按钮</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Second",
+        data:function () {
+            return {
+                username:'小黑',
+                count:80,
+            }
+        },
+        methods:{
+            change(){
+                // alert('触发了事件');
+                this.count--;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<!--组件的template 内部只能有一个根标签-->
+<template>
+    <!--    不能有同级的标签-->
+    <div>
+        这是我的第一个组件====组件使用成功
+        <hr>
+        <span>这是一节多么快乐而又优秀的课呀！！</span>
+    </div>
+</template>
+
+<!--编写js代码的地方 当前对象就是组件对象-->
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
+
+<!--用来为当前组件指定样式 scoped:脚手架提供的属性 加上此属性代表当前样式只在当前组件生效-->
+<style scoped>
+    div {
+        color: green;
+    }
+
+    span {
+        color: darkmagenta;
+    }
+</style>
